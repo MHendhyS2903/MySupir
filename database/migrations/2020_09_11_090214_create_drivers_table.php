d9e9e00d029724fb51ffbb57ea6fd9bae07ac144
Hendhy <m.hendhy.s@gmail.com>
2020-09-11 16:06:29 +0700

    First Commit

---
+++ database/migrations/2020_09_11_090214_create_drivers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDriversTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->increments('driverID');
+            $table->string('name');
+            $table->string('nohp');
+            $table->text('address');
+            $table->string('sim');
+            $table->text('photo');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+    }
+}
