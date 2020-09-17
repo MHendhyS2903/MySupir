279086eafcc5f8aba7a9d56f25875f1b662abf18
joshuaprima <joshuaprimap@gmail.com>
2020-09-17 15:15:42 +0700

    fix driverloc

--- app/Models/DriverLoc.php
+++ app/Models/DriverLoc.php
@@ -8,18 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class DriverLoc extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
 
-    protected $table="driver_loc";
-    // protected $primaryKey="driverLocID";
-
-    protected $fillable = [
-        'driverID', 'location', 'lang', 'lat',
-    ];
-
-    public function driver(){
-    	return $this->belongsTo('App\Models\Driver', 'driverID');
-=======
     protected $table="driver_loc";
     protected $primaryKey="driverLocID";
 
@@ -29,6 +18,5 @@ class DriverLoc extends Model
 
     public function driver(){
         return $this->belongsTo('App\Models\Driver', 'driverID');
->>>>>>> 3710203e2aa5b5116d5c295296d2fc19349caabd
     }
 }
